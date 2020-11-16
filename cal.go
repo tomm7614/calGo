b11c9a5fc319e4492af78e4f1ebf2f378168baae
tomm <somjeth@gmail.com>
2020-11-16 18:00:48 +0700

    1st

---
+++ cal.go
@@ -0,0 +1,16 @@
+package cal
+
+import(
+	"github.com/tomm7614/minusGo"
+	"github.com/tomm7614/plusGo"
+)
+
+//Plus func
+func Plus(a float64, b float64) float64{
+	return plus.Plus(a, b)
+}
+
+//Minus func
+func Minus(a float64, b float64) float64{
+	return minus.Minus(a, b)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module cal
+
+go 1.15
+
+require (
+	github.com/tomm7614/minusGo v0.0.0-20201116104819-85dfad1269ab
+	github.com/tomm7614/plusGo v0.0.0-20201116104603-0308adb0c573
+)
